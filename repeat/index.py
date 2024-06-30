1bfab3f96792422003816aa7eb2ef7e946def2c5
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-06-30 22:52:20 +0500

    deleting fc file

---
+++ repeat/index.py
@@ -0,0 +1,96 @@
+# print('Hello', 'Roziya', 'U', 'yaxshi', end=' odam', sep='__')
+# print(' World')
+
+# end oxirida chiqadigan string
+# sep , lar orasi nima bilan ajralishi
+
+
+# ---------------
+
+# text = 'Roziya Tukhtayeva'
+
+# for x in text:
+#     if(x == 'a'):
+#         print('.')
+#     else:
+#         print(x)
+
+# obj = [-34, 45, 89]
+
+# for o in obj:
+#     if (o > 0):
+#         print(o)
+
+# it is just like js's for
+
+
+# ---------------
+
+
+# name = input('What is your name? ')
+# print('Thanks', name)
+
+# it is just input
+
+
+# ---------------
+
+
+# total = 0
+
+# while input('Continue ? (Y/N): ') == 'Y':
+#     qty = int(input('Enter quantiti: '))
+#     if(qty < 0):
+#         break
+#     total += qty
+# else:
+#     print(total)
+
+# if qty is negative this just finish
+
+
+# ---------------
+
+
+# a = 0.1
+# b = 0.2
+# print(a+b)
+
+# computer take 0.1 binary float number so it will become 0.1000435435...
+
+
+# ---------------
+
+
+# total = 0
+# for x in range(50):
+#     total += x
+#     if x>2:
+#         break
+# print(total) 
+
+
+# ---------------
+
+# name = 'Roziya'
+
+# surname = 'Tukhtayeva ' + name
+
+# print(surname)
+# print(surname.count('a'))
+# print(surname.replace('Roziya', 'TR'))
+
+# txt = '                      T                   '
+# print(txt)
+# print(txt.strip())
+# print(txt.lstrip(), end='/')
+
+
+# ---------------
+
+
+count = 0
+while count < 10:
+    count += 1
+    print(count)
+print('End.')
